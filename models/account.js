448d4f405cdf081c3299fd5bc30775f0168df05d
Lushnia Kateryna <71783791+katyalushnya@users.noreply.github.com>
2022-09-26 01:33:13 +0300

    Add files via upload

---
+++ models/account.js
@@ -0,0 +1,9 @@
+export class Account {
+    constructor(name, token, address, email, id) {
+        this.name = name;
+        this.token = token;
+        this.address = address;
+        this.email = email;
+        this.id = id;
+    }
+}
